0962bd2f278666df820e4d4ecb88207ca408233a
pressystar <pifebuche81@gmail.com>
2023-06-15 21:26:53 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main -6-size.c
+ * description -Write a C program that prints
  *
- * Return: 0 (Success)
- */int main(void)
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
 char a;
 int b;
 long int c;
 long long int d;
 float f;
-printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
-printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
-printf("size of long int: %lu bytes(s)\n,(unsigned long)sizeof(c));
-printf("size of a long long int: %lu bytes(s)\n",(unsigned long)sizeof(d));
-printf("size of a float: %lu bytes(s)\n",(unsigned long)sizeof(f));
+
+printf("size of a char: %lu byte(s)\n", sizeof(char));
+printf("size of an int: %lu byte(s)\n", sizeof(int));
+printf("size of long int: %lu bytes(s)\n,sizeof(long int));
+printf("size of a long long int: % lu bytes(s)\n", sizeof(long long int));
+printf("size of a float : % lu bytes(s)\n", sizeof(float));
+
 return (0);
 }
