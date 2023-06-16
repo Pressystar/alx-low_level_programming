7ce9bc62c9f2049e3acbedb09989155a0f74a97d
pressystar <pifebuche81@gmail.com>
2023-06-16 16:03:22 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+char b;
+b = 'a';
+while
+(b <= 'z') {
+putchar (b);
+b++;
+}
+putchar('\n');
+return (0);
+}
