feafac4b7c4530a1ff483383ee1f6f378a1ad89e
pressystar <pifebuche81@gmail.com>
2023-06-17 15:37:20 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
  * main - Entry point
  * Return: 0 (Success)
  */
 int main(void)
 {
-
-int g;
-
-for (g = '0'; g <= '9'); g++;
-
-putchar(g);
+char low;
+for (low = 'z'; low >= 'a'; low--)
+putchar(low);
 putchar('\n');
 return (0);
 }
