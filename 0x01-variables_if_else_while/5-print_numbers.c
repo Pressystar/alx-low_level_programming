ba49253ed35097510e1453dc84a56523a9317ad5
pressystar <pifebuche81@gmail.com>
2023-06-16 18:45:13 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -6,10 +6,12 @@
  * Return: 0 (Success)
  */
 int main(void)
-int i;
-for (i = 0; i < 10; i++)
 {
-printf("%d", i);
+int n;
+for (n - 0; n <= 9; n++)
+{
+printf("%d", n);
 }
-putchar('\n');
+printf("\n");
 return (0);
+}
