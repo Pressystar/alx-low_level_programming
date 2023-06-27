83f30517b4f0fa2e7ec546a679111c12e45aa827
pressystar <pifebuche81@gmail.com>
2023-06-27 08:10:26 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -7,39 +7,25 @@
  * Return: 0
  */
 int main(void)
-{       
-char password[84];
+{
+int pass[100];
+int j, sum, n;
 
-int index = 0, sum = 0, diff_half1, diff_half2;
-	srand(time(0));
-	while (sum < 2772)
-	{
-	password[index] = 33 + rand() % 94;
-sum += password[index++];
-}
-password[index] = '\0';
-if (sum != 2772)
+sum =    0;
+
+srand(time(NULL));
+for (j = 0; j < 100; j++)
 {
-diff_half1 = (sum - 2772) / 2;
-diff_half2 = (sum - 2772) / 2;
-      
-if ((sum - 2772) % 2 != 0)
-diff_half1++;
-for (index = 0; password[index]; index++)
+pass[j] = rand() % 78;
+sum += (pass[j] + '0');
+putchar(pass[j] + '0');
+if ((2772 - sum) -'0' < 78)
 {
-if (password[index] >=(33 + diff_half1))	
-{
-password[index] -= diff_half1;
-break;
-}
-for (index = 0; password[index]; index++)
-{
-if (password[index] >= (33 + diff_half2))
-password[index] -= diff_half2;
+n = 2772 - sum - '0';
+sum += n;
+putchar(n + '0')'
 break;
 }
 }
-}
-printf("%s", password);
 return (0);
 }
