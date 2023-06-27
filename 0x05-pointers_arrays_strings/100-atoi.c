6852e8c584b4be7cc7826895e016a3c4d8c755c3
pressystar <pifebuche81@gmail.com>
2023-06-27 05:27:55 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,7 +11,7 @@ int i, d, n, len, f, digit;
 i = 0;
 d = 0;
 n = 0;
-len = 0; 
+len = 0;
 f = 0;
 digit = 0;
 
@@ -28,7 +28,7 @@ if (d % 2)
 digit = -digit;
 n = n * 10 + digit;
 f = 1;
-if (s[i + 1] < '0' || s[i + 1] > '0')
+Aif (s[i + 1] < '0' || s[i + 1] > '9')
 break;
 f = 0;
 }
