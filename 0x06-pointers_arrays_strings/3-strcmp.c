1ba4ef378dfa7beec7d94dac108971ad74f20099
pressystar <pifebuche81@gmail.com>
2023-06-29 11:17:34 -0700

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include"main.h"
+
+/**
+ * _strcmp - compare two string
+ * @s1: string 1
+ * @s2: string 2
+ * Return: difference s1[i] - s2[i]
+ */
+int _strcmp(cha *s1, char *s2)
+{
+int i;
+
+i = 0;
+while (s1[i] != '\0' && s2[i] != '\0')
+{
+if (s1[i] != s2[i])
+{
+return (s1[i] - s2[i]);
+}
+i++;
+}
+return (0);
+}
+
+
