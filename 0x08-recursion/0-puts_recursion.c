c8ba869a3a3b62183d29198b13957c99c952ad76
pressystar <pifebuche81@gmail.com>
2023-07-04 23:43:01 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,8 +1,8 @@
 #include"main.h"
 /**
- * _puts-recursion - Write a function that prints a string
+ * _puts_recursion - Write a function that prints a string
  *  @s: input
- *  Return: 0
+ *  Return: 0 (Success)
  */
 void _puts_recursion(char *s)
 {
