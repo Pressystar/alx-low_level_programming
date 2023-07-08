19e31e1b3704d51e0bbcc7e84159938286b80125
pressystar <pifebuche81@gmail.com>
2023-07-08 16:36:35 -0700

    create_static_lib.sh

--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -8,9 +8,9 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-int q;
+int q = 0;
 
-while (q  < n && src[q] != '\0')
+while (q < n && src[q] != '\0')
 {
 dest[q] = src[q];
 q++;
@@ -19,6 +19,6 @@ while (q < n)
 dest[q] = '\0';
 q++;
 }
-return (dest);
 }
+return (dest);
 }
