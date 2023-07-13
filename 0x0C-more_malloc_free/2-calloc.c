fe23d522b1ce2df38594f6d0df81efc863f287fc
pressystar <pifebuche81@gmail.com>
2023-07-13 08:26:05 -0700

    2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,38 @@
+#include"main.h"
+#include<stdlib.h>
+
+
+/**
+ * _calloc - Entry  point
+ * @nmemb: input value
+ * @size: input value
+ * Return: input value
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+int a = 0, b = 0;
+char *x;
+
+if (nmemb == 0 || size == 0)
+return (NULL);
+
+b = nmemb * size;
+x = malloc(b);
+if (x == NULL)
+return (NULL);
+
+b = nmemb * size;
+x = malloc(b);
+
+if (x == NULL)
+return (NULL);
+
+while (a < b)
+{
+x[a] = 0;
+a++;
+}
+return (x);
+}
+
+
