dd0244c7b489522ad40755c98640d69d30242c7c
pressystar <pifebuche81@gmail.com>
2023-07-18 08:15:51 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -17,5 +17,5 @@ e->name = "(nil)";
 if (e->owner == NULL)
 e->owner = "(nil)";
 
-printf("Name: %s\nAge: %f\nowner: %s\n", e->name, e->age, e->owner);
+printf("Name: %s\nAge: %f\nOwner: %s\n", e->name, e->age, e->owner);
 }
