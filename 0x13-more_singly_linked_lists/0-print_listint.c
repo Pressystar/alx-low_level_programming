42108865fc4d40952f4ef682b7093cb7bbb40d47
precious <pifebuche81@gmail.com>
2023-08-02 00:22:35 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,13 +8,12 @@
  */
 size_t print_listint(const listint_t *h)
 {
-size_t p = 0;
+size_t sum = 0;
 
 while (h)
 {
-p++;
-printf("%d\n", h->n);
+sum++;
 h = h->next;
 }
-return (p);
+return (sum);
 }
