21150b797c855367c785aea2bfdf245223f935b2
precious <pifebuche81@gmail.com>
2023-08-08 08:20:19 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,7 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
+
 
 char *create_buffer(char *file);
 void close_file(int fd);
@@ -14,7 +15,7 @@ char *create_buffer(char *file)
 {
 char *buffer;
 
-buffer = malloc(sizeof(char) *1024);
+buffer = malloc(sizeof(char) * 1024);
 
 if (buffer == NULL)
 {
@@ -30,10 +31,10 @@ return (buffer);
  */
 void close_file(int fd)
 {
-int c;
-c = close(fd);
+int g;
+g = close(fd);
 
-if (c == -1)
+if (g == -1)
 {
 dprintf(STDERR_FILENO, "Error: can't close fd %d\n", fd);
 exit(100);
@@ -66,8 +67,7 @@ from = open(argv[1], O_RDONLY);
 r = read(from, buffer, 1024);
 to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
-do
-{
+do{
 if (from == -1 || r == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
