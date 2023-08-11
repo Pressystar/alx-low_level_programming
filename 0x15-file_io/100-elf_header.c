f8defadfefb3721fcb9ff1c68f81788abcf918e5
precious <pifebuche81@gmail.com>
2023-08-11 08:35:45 -0700

    100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -267,14 +267,14 @@ o = open(argv[1], O_RDONLY);
 
 if (o == -1)
 {
-dprintf(STDERR_FILENO, "Error:Can't read file %s\n", argv[1]);
+dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
 exit(98);
 }
 header = malloc(sizeof(Elf64_Ehdr));
 if (header == NULL)
 {
 close_elf(o);
-dprintf(STDERR_FILENO, "Error:can't read file %s\n", argv[1]);
+dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
 exit(98);
 }
 r = read(o, header, sizeof(Elf64_Ehdr));
